d9c6a85fa4840a4ab4bec1a4eb346b7f65d79089
Colt Borg <colt.borg@backstopsolutions.com>
2017-08-03 16:48:44 -0500

    5.5 Homework

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,21 +1,14 @@
 <template>
   <div class="hello">
-    <h1>Type 2 numbers and choose operation.</h1>
-    <form>
-      <input type="text" v-model.number="a">
-      <select v-model="operator" @change="calculate">
-        <option>+</option>
-        <option>-</option>
-        <option>*</option>
-        <option>/</option>
-      </select>
-      <input type="text" v-model.number="b">
-      <button type="submit" @click.prevent="calculate">Calculate</button>
-    </form>
-    <h2>Result: {{ a }} {{ operator }} {{ b }} = {{ c }}</h2>
-    <pre>
-      {{ $data }}
-    </pre>
+    <h1>People of Vue</h1>
+    <ul>
+      <li v-for="person in people" :key="person.name">{{ person.name }} ({{ person.votes }}) <button type="button" @click="person.votes++">Vote</button></li>
+    </ul>
+    <input type="text" placeholder="Enter delete to reset">
+    <h1>Our Mayor is {{ winner.name }}</h1>
+
+    <pre>{{ $data }}</pre>
+    <pre>{{ winner }}</pre>
   </div>
 </template>
 
@@ -24,36 +17,35 @@ export default {
   name: 'hello',
   data() {
     return {
-      a: 1,
-      b: 2,
-      c: null,
-      operator: '+',
+      people: [{
+        name: 'Kevin',
+        votes: 0,
+      }, {
+        name: 'Mike',
+        votes: 0,
+      }, {
+        name: 'Lisa',
+        votes: 0,
+      }, {
+        name: 'Colt',
+        votes: 0,
+      }],
     };
   },
-  methods: {
-    calculate() {
-      switch (this.operator) {
-        case '+':
-          this.c = this.a + this.b;
-          break;
-        case '-':
-          this.c = this.a - this.b;
-          break;
-        case '*':
-          this.c = this.a * this.b;
-          break;
-        case '/':
-          this.c = this.a / this.b;
-          break;
-        default:
-          break;
-      }
+  computed: {
+    winner() {
+      const compareVotes = this.people.sort((a, b) => b.votes - a.votes);
+      return compareVotes[0];
     },
   },
+  methods: {},
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+pre {
+  background-color: #e0e0e0;
+  padding: 0.5rem;
+}
 </style>
