b57227309f93f91fc469a4174f45b33bdbcb4bb1
legendofcolt <coltborg@gmail.com>
2017-08-09 17:33:17 -0500

    6.3 Custom Filters

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,14 +1,13 @@
 <template>
   <div class="hello">
-    <h1>People of Vue</h1>
+    <h1>Real super hero identities</h1>
     <ul>
-      <li v-for="person in people" :key="person.name">{{ person.name }} ({{ person.votes }}) <button type="button" @click="person.votes++">Vote</button></li>
+      <li v-for="hero in heros" :key="hero">
+        {{ hero | snitch }}
+      </li>
     </ul>
-    <input type="text" placeholder="Enter delete to reset">
-    <h1>Our Mayor is {{ winner.name }}</h1>
 
     <pre>{{ $data }}</pre>
-    <pre>{{ winner }}</pre>
   </div>
 </template>
 
@@ -17,26 +16,18 @@ export default {
   name: 'hello',
   data() {
     return {
-      people: [{
-        name: 'Kevin',
-        votes: 0,
-      }, {
-        name: 'Mike',
-        votes: 0,
-      }, {
-        name: 'Lisa',
-        votes: 0,
-      }, {
-        name: 'Colt',
-        votes: 0,
-      }],
+      heros: [
+        {
+          firstName: 'Bruce',
+          lastName: 'Wayne',
+          secretId: 'Batman',
+        }, {
+          firstName: 'Clark',
+          lastName: 'Kent',
+          secretId: 'Superman',
+        },
+      ],
     };
-  },
-  computed: {
-    winner() {
-      const compareVotes = this.people.sort((a, b) => b.votes - a.votes);
-      return compareVotes[0];
-    },
   },
   methods: {},
 };
--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import App from './App';
 
 Vue.config.productionTip = false;
 
+Vue.filter('snitch', hero => `${hero.secretId} is ${hero.firstName} ${hero.lastName} in real life`);
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
